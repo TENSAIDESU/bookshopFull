24a2106a84bd8e5e6384f6ba5636a7ba1bc57b7f
TENSAIDESU <140738963+TENSAIDESU@users.noreply.github.com>
2024-04-17 19:40:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import { code } from './src/js/code.js';
+import { loader } from './src/js/loader.js';
+import {buttonbag} from './src/js/buttonbag.js';
+import {burgermenu} from '.src/js/burger-menu.js';
+code();
+loader();
+buttonbag();
+burgermenu();
+
