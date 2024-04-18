9233fbfa829d9816c4fa898910243083e3eb1da4
TENSAIDESU <140738963+TENSAIDESU@users.noreply.github.com>
2024-04-18 18:26:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { code } from './src/js/code.js';
 import { loader } from './src/js/loader.js';
-import {buttonbag} from './src/js/buttonbag.js';
+import {buttonbag} from './src/js/button-bag.js';
 import {burgermenu} from '.src/js/burger-menu.js';
 code();
 loader();
