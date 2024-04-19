40b982fc2a50894f66720b8004369982b5abe77d
TENSAIDESU <140738963+TENSAIDESU@users.noreply.github.com>
2024-04-19 18:22:00 +0300

    Add files via upload

--- src/js/loader.js
+++ src/js/loader.js
@@ -2,7 +2,7 @@ let loader = () => {
 const API_Key='AIzaSyDszvaaBOPGUYhjJ5L2m_OvNUPs6U6CH78';
 const GET_COUNT_BOOKS= 6;
 let currentStep = 0;
-let currentSubject = document.querySelector('.nav-category-list__item').getAttribute('data-category');
+let currentSubject = document.querySelector('.link').getAttribute('data-category');
 
 
 const useRequestBooks =(subject, start, count) => {
@@ -26,7 +26,7 @@ const getAndShowBooks = async () => {
     let authors = book.volumeInfo?.authors ?? 'No authors';
     let title = book.volumeInfo.title;
     let description = book.volumeInfo.description ?? 'no describtion';
-    let averageRating = book.volumeInfo?.averageRating ?? '';
+    
     let saleability = book.saleInfo.saleability;
     let price = "No for sale";
     let priceType = "";
@@ -42,7 +42,7 @@ const getAndShowBooks = async () => {
       priceType = book.saleInfo.retailPrice?.currencyCode;
     }
     const newBook = `
-    <div class="card_item">
+    <div class="card_item" id="${book.id}">
     <div class ="card_item__image"><img class="holder" src="${img} "></div>
       <div class="desribtion">
          <div class="card_item__author">${authors}</div>
@@ -62,7 +62,7 @@ const getAndShowBooks = async () => {
         <div class="card_item__price">${price}</div>
         <div class="card_item__priceType">${priceType}</div>
         </div>
-      <button class="buy-btn" onclick="toggleCart(this)">Buy now</button> 
+      <button class="buybtn" id="${book.id}">Buy now</button> 
       </div>
     `
     document.querySelector('.card').innerHTML += newBook;
@@ -78,6 +78,41 @@ for (let i = 0; i < rating; i++) {
   })
 }
 
+const buttons = Array.from(document.querySelectorAll('.buybtn'));
+const container = document.querySelector('.cart-counter');
+
+buttons.forEach(button => {
+if (Object.prototype.hasOwnProperty.call(localStorage, button.id)) {
+    button.classList.add("added");
+    button.innerHTML = "IN THE CART";
+}
+
+let bagItems = `<div class="items-number">${Object.keys(localStorage).length}</div>`;
+if (Object.keys(localStorage).length > 0) {
+    container.innerHTML = bagItems;
+}
+let cartCounter = 0;
+button.addEventListener("click", () => {
+    const idName = button.id;
+    if (!button.classList.contains("added")) {
+    button.innerHTML = "IN THE CART";
+    button.classList.add("added");
+    localStorage.setItem(idName, button.id);
+    cartCounter--;
+    } else if (button.classList.contains("added")) {
+    button.innerHTML = "BUY NOW";
+    button.classList.remove("added");
+    localStorage.removeItem(idName, button.id);
+    cartCounter++;
+    }
+    bagItems = `<div class="items-number">${Object.keys(localStorage).length}</div>`;
+    container.innerHTML = bagItems;
+    if (Object.keys(localStorage).length == 0) {
+    container.innerHTML = " ";
+    }
+    document.getElementById('cart-counter').textContent = cartCounter;   
+});
+});
 
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('link')) {
