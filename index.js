18cff97648120f0614595b10a656de721e6aa2af
TENSAIDESU <140738963+TENSAIDESU@users.noreply.github.com>
2024-04-23 17:19:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { code } from './src/js/code.js';
 import { loader } from './src/js/loader.js';
 import {burgermenu} from './src/js/burger-menu.js';
+import './style.scss';
 code();
 loader();
 burgermenu();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
-//const HtmlWebpackPlugin = require('html-webpack-plugin');
-//const ESLintPlugin =require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry:   path.resolve(__dirname, 'index.js'),
@@ -8,5 +10,48 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'index.main.js',
     },
-   
-        }
+    mode: 'production',
+    plugins: [  new MiniCssExtractPlugin(), 
+                new HtmlWebpackPlugin(
+                {path: path.resolve(__dirname, './dist'),
+                  template: './src/html/index.pug',
+                  filename: 'index.html',
+                  assetModuleFilename: path.join('images', '[name].[contenthash][ext]')
+                }
+                ),
+            ], 
+    module: {
+        rules: [
+          {
+            test: /\.scss/,
+                use: [MiniCssExtractPlugin.loader,"css-loader"],
+          }, 
+          {
+            test: /\.pug$/,
+            loader: 'pug-loader',
+            options: {
+            pretty: true
+            }
+          },
+          {
+            test: /\.(png|jpg|jpeg|)$/i,
+              type: 'asset/resource',
+                  },
+                   {
+                     test: /\.svg$/,
+                     type: 'asset/resource',
+                     generator: {
+                       filename: path.join('icons', '[name].[contenthash][ext]'),
+                     },
+                   }
+                    ],
+      },
+      optimization: {
+        minimizer: [
+          `...`,
+          new CssMinimizerPlugin(),
+             
+              ],
+
+             },
+            };
