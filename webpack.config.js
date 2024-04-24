7790ddb31e64d8cdee695c035288fb2e44de8e32
TENSAIDESU <140738963+TENSAIDESU@users.noreply.github.com>
2024-04-24 17:28:39 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    entry:   path.resolve(__dirname, 'index.js'),
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index.main.js',
+  entry: path.resolve(__dirname, './index.js'),
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js'
+  },
+  mode: 'production',
+  plugins: [new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin(
+          {
+              template: './src/html/index.pug',
+              filename: 'index.html'
+          }
+      ),
+      new CopyPlugin({
+          patterns: [
+            { from: "./src/images", to: "src/images" },
+          ],
+        }),
+],
+  module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-loader',
+          options: {
+          pretty: true
+          }
+      }
+      ],
+   
+  },
+  optimization: {
+      minimizer: [
+        `...`,
+        new CssMinimizerPlugin(),
+      ],
     },
-    mode: 'production',
-    plugins: [  new MiniCssExtractPlugin(), 
-                new HtmlWebpackPlugin(
-                {path: path.resolve(__dirname, './dist'),
-                  template: './src/html/index.pug',
-                  filename: 'index.html',
-                  assetModuleFilename: path.join('images', '[name].[contenthash][ext]')
-                }
-                ),
-            ], 
-    module: {
-        rules: [
-          {
-            test: /\.scss/,
-                use: [MiniCssExtractPlugin.loader,"css-loader"],
-          }, 
-          {
-            test: /\.pug$/,
-            loader: 'pug-loader',
-            options: {
-            pretty: true
-            }
-          },
-          {
-            test: /\.(png|jpg|jpeg|)$/i,
-              type: 'asset/resource',
-                  },
-                   {
-                     test: /\.svg$/,
-                     type: 'asset/resource',
-                     generator: {
-                       filename: path.join('icons', '[name].[contenthash][ext]'),
-                     },
-                   }
-                    ],
-      },
-      optimization: {
-        minimizer: [
-          `...`,
-          new CssMinimizerPlugin(),
-             
-              ],
-
-             },
-            };
+    
+}; 
